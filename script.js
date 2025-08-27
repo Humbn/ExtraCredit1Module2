cfdaa7180f9eac9331c3427559e38edad65b8f9d
Roberto <rduran2004@icloud.com>
2025-08-27 10:59:10 -0500

    not done

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 // Select the form using querySelector
-const form="Your code instead of this string";
+const form = document.querySelector("form");
 
 // TODO: Attach a "submit" event listener to the form
 //Start of listener
+form.addEventListener = ("submit", function(event) {
+  
+});
   // TODO: Prevent the form from refreshing the page
+  Event.preventDefault();
   
   /* 1. Show parent/children Data for learning/debugging */
   
   // TODO: Log the form's children collection in the console
+  console.log(form.children);
   // TODO: Log the form's parent element tag name
+  console.log()
   // TODO: Log how many children the form has
 
   /* 2. Extract values */
   // TODO: Select the name input
-  const nameInput = "Your code instead of this string";
+  const nameInput = "";
+
 
   // TODO: Select the satisfaction input by its id
   const satisfactionInput = "Your code instead of this string";
